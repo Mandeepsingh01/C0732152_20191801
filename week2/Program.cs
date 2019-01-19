d1c44f0beef4507236a77990cef54dd492a33f0d
visualstudio <visualstudio@122-PC19>
2019-01-18 21:00:59 -0500

    commit1

--- week2/Program.cs
+++ week2/Program.cs
@@ -7,9 +7,35 @@ using System.Threading.Tasks;
 namespace week2
 {
     class Program
-    {
-        static void Main(string[] args)
         {
+            static void Main(string[] args)
+            {
+
+                StudentInformation _Mary = new StudentInformation("Mary", 3.5, 20);
+                StudentInformation _John = new StudentInformation("John", 2.0, 10);
+                StudentInformation _Mark = new StudentInformation("Mark", 0.2, 3);
+                StudentInformation _steve = new StudentInformation("Steve", 2.9, 16);
+                StudentInformation _Cindy = new StudentInformation("Cindy", 2.8, 18);
+            }
         }
-    }
+
+        class StudentInformation
+        {
+            public string NAME;
+            public string StudentNumber;
+            public double GPA;
+            public int Attendence;
+            public StudentInformation(string aNAME, double cGPA, int dAttendence)
+
+            {
+                NAME = aNAME;
+                GPA = cGPA;
+                Attendence = dAttendence;
+
+
+            }
+        }
+
+    
 }
+
